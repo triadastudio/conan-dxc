54cf5b170b923d8cfcfe77c3f9d54033dab0182a
Bagrat Dabaghyan <dbagrat@gmail.com>
2021-12-14 00:14:44 +0400

    PR candidate

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class DXCConan(ConanFile):
     name = "dxc"
     version = "1.6.2112"
     description = "DirectX Shader Compiler"
-    license = "LLVM"
-    topics = ("hlsl", "dxc")
+    license = "NCSA"
+    topics = ("hlsl", "dxc", "compiler", "shader", "spirv")
     homepage = "https://github.com/microsoft/DirectXShaderCompiler"
     url = "https://github.com/triadastudio/conan-dxc"
     settings = "os", "arch", "compiler", "build_type"
@@ -42,21 +42,21 @@ class DXCConan(ConanFile):
     def build_windows(self):
         win_sdk_ver = "10.0.20348.0"
         self.run("call utils/hct/hctstart.cmd . %s && "
-                 "call utils/hct/hctbuild.cmd -x64 -Release -dxc-cmake-system-version %s -spirv -show-cmake-log"
-                 % (self.build_folder, win_sdk_ver), cwd=self._source_dir)
+                 "call utils/hct/hctbuild.cmd -x64 -%s -dxc-cmake-system-version %s -spirv -show-cmake-log"
+                 % (self.build_folder, self.settings.build_type, win_sdk_ver), cwd=self._source_dir)
 
     @property
     def _predefined_cmake_params_path(self):
         return os.path.join(self._source_dir, "cmake/caches/PredefinedParams.cmake")
 
     def build_linux(self):
-        self.run("cmake . -B%s -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -C %s" %
-                 (self.build_folder, self._predefined_cmake_params_path), cwd=self._source_dir)
+        self.run("cmake . -B%s -GNinja -DCMAKE_BUILD_TYPE=%s -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -C %s" %
+                 (self.build_folder, self.settings.build_type, self._predefined_cmake_params_path), cwd=self._source_dir)
         self.run("ninja -j 4")
 
     def build_macos(self):
-        self.run("cmake . -B%s -GNinja -DCMAKE_BUILD_TYPE=Release -C %s" %
-                 (self.build_folder, self._predefined_cmake_params_path), cwd=self._source_dir)
+        self.run("cmake . -B%s -GNinja -DCMAKE_BUILD_TYPE=%s -C %s" %
+                 (self.build_folder, self.settings.build_type, self._predefined_cmake_params_path), cwd=self._source_dir)
         self.run("ninja")
 
     def build(self):
@@ -83,4 +83,8 @@ class DXCConan(ConanFile):
             self.copy("bin/dxc", dst="bin", keep_path=False)
 
     def package_info(self):
+        self.cpp_info.names["cmake_find_package"] = "dxc"
+        self.cpp_info.names["cmake_find_package_multi"] = "dxc"
+        self.cpp_info.names["pkg_config"] = "dxc"
         self.cpp_info.libs = ["dxcompiler"]
+        self.cpp_info.includedirs = ["include"]
