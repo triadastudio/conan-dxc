f854f02aed376ec3ff496a5c49b83d355f44297f
Bagrat Dabaghyan <dbagrat@gmail.com>
2024-03-14 21:33:59 +0400

    Update to the DXC v1.8.2403 and fix windows build

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ import os
 
 class DXCConan(ConanFile):
     name = "dxc"
-    version = "1.7.2308"
+    version = "1.8.2403"
     description = "DirectX Shader Compiler"
     license = "NCSA"
     topics = ("hlsl", "dxc", "compiler", "shader", "spirv")
@@ -18,7 +18,7 @@ class DXCConan(ConanFile):
 
     @property
     def _source_commit_or_tag(self):
-        return "v1.7.2308"
+        return "v1.8.2403"
 
     @property
     def _source_subfolder(self):
@@ -38,16 +38,14 @@ class DXCConan(ConanFile):
             self.run(f"git checkout {self._source_commit_or_tag}")
             self.run("git submodule update --init --recursive")
 
-    def build_windows(self):
-        win_sdk_ver = "10.0.20348.0"
-
-        self.run("call utils/hct/hctstart.cmd . %s && "
-                 "call utils/hct/hctbuild.cmd -x64 -%s -dxc-cmake-system-version %s -spirv -show-cmake-log"
-                 % (self.build_folder, self._build_type, win_sdk_ver), cwd=self._source_dir)
-
     @property
     def _predefined_cmake_params_path(self):
         return os.path.join(self._source_dir, "cmake/caches/PredefinedParams.cmake")
+
+    def build_windows(self):
+        self.run("cmake . -B%s -A x64 -DCMAKE_BUILD_TYPE=%s -C %s" %
+                 (self.build_folder, self._build_type, self._predefined_cmake_params_path), cwd=self._source_dir)
+        self.run("cmake --build %s --target \"dxc\" --config Release" % self.build_folder )
 
     def build_linux(self):
         self.run("cmake . -B%s -GNinja -DCMAKE_BUILD_TYPE=%s -DCMAKE_C_COMPILER=clang-13 -DCMAKE_CXX_COMPILER=clang++-13 -C %s" %
